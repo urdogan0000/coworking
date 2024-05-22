1741c6a058c1f109230f505b775fb8ec462bb84f
sgnm-haydar <haydar.urdogan@signumtte.com>
2024-05-22 21:46:03 +0300

    contact page added

--- FrontEnd/src/views/AboutView.vue
+++ FrontEnd/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">About Page</h1>
+  <div class="p-6 flex justify-center ">
+    <div>
+      <h1 class="text-2xl font-bold">About Page</h1>
     <p>Learn more about us here.</p>
+    </div>
+ 
   </div>
 </template>
---
+++ FrontEnd/src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{ compName }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const compName = ref("Contact Page");
+</script>
+
+<style scoped></style>
