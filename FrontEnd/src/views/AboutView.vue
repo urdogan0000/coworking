3e1a2a286d801d963a752db7222fbf1ba79b4123
sgnm-haydar <haydar.urdogan@signumtte.com>
2024-05-22 11:52:25 +0300

    projext initialized

---
+++ FrontEnd/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">About Page</h1>
+    <p>Learn more about us here.</p>
+  </div>
+</template>
---
+++ FrontEnd/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Home Page</h1>
+    <p>Welcome to the home page.</p>
+  </div>
+</template>
